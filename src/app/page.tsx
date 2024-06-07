14f4c2397a7e67f370ceaf0e68bdad1f9f8482c9
makarella-v <v.makarenko@svitla.com>
2024-06-07 14:05:13 +0300

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { Button } from '@/features/shared/ui/button';
 
+import Link from 'next/link';
+
 export default function Page() {
   return (
-    <main className="flex h-screen items-center justify-center">
-      <div className="flex flex-col gap-6">
-        <Button>Default</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button variant="outline">Outline</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="ghost">Ghost</Button>
-      </div>
+    <main className="flex h-screen flex-col items-center justify-center gap-12">
+      <h1 className="bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text text-4xl font-bold text-transparent md:text-7xl">
+        Jobio
+      </h1>
+      <p className="text-3xl">
+        Job tracking app for <span className="font-bold">job hunters</span>
+      </p>
+      <Button className="p-6 text-xl" asChild>
+        <Link href="/jobs/add">Get Started</Link>
+      </Button>
     </main>
   );
 }
