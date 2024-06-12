420e0e49d9f663c7996a2f67de0fb449d3064f00
makarella-v <v.makarenko@svitla.com>
2024-06-12 16:53:19 +0300

    setup job list component

--- src/features/dashboard/ui/job-list.tsx
+++ src/features/dashboard/ui/job-list.tsx
@@ -1,3 +1,37 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { getAllJobs } from '@/features/dashboard/data-access';
+import { useSearchParams } from 'next/navigation';
+import { JobCard } from '@/features/dashboard/ui/job-card';
+
 export function JobList() {
-  return <div>Job List</div>;
+  const searchParams = useSearchParams();
+
+  const search = searchParams.get('search') ?? '';
+  const jobStatus = searchParams.get('status') ?? '';
+  const page = Number(searchParams.get('page')) || 1;
+
+  const { data, isPending } = useQuery({
+    queryKey: ['jobs', search, jobStatus, page],
+    queryFn: () => getAllJobs({ search, jobStatus, page }),
+  });
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  const jobs = data?.jobs ?? [];
+
+  if (jobs.length === 0) {
+    return <div>No jobs found...</div>;
+  }
+
+  return (
+    <div className="grid gap-8 md:grid-cols-2">
+      {jobs.map((job) => (
+        <JobCard key={job.id} job={job} />
+      ))}
+    </div>
+  );
 }
--- src/features/dashboard/ui/search-form.tsx
+++ src/features/dashboard/ui/search-form.tsx
@@ -11,16 +11,30 @@ import {
 import { JobStatus } from '@/features/dashboard/domain';
 import { Button } from '@/features/shared/ui/button';
 import React from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export function SearchForm() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const searchParams = useSearchParams();
+
+  const search = searchParams.get('search') ?? '';
+  const jobStatus = searchParams.get('status') ?? 'all';
+
   function handleSearch(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    const params = new URLSearchParams();
 
     const formData = new FormData(e.currentTarget);
     const search = formData.get('search') as string;
     const jobStatus = formData.get('job-status') as string;
 
-    console.log(search, jobStatus);
+    params.set('search', search);
+    params.set('status', jobStatus);
+
+    router.push(`${pathname}?${params.toString()}`);
   }
 
   return (
@@ -28,8 +42,8 @@ export function SearchForm() {
       className="mb-16 grid gap-4 rounded-lg bg-muted p-8 sm:grid-cols-2 md:grid-cols-3"
       onSubmit={handleSearch}
     >
-      <Input placeholder="Search jobs..." name="search" />
-      <Select name="job-status">
+      <Input placeholder="Search jobs..." name="search" defaultValue={search} />
+      <Select name="job-status" defaultValue={jobStatus}>
         <SelectTrigger>
           <SelectValue />
         </SelectTrigger>
